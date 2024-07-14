b3ee51276c19e4122cf617a2ab167309121f457c
Ox-Harris <ox-harris@Ox-Harris-MacBook-Pro.local>
2024-07-14 12:10:18 +0100

    ...

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-export default function( event, data, next ) {
+
+import SQL from '@linked-db/linked-ql/sql';
+import pg from 'pg';
+const pgClient = new pg.Client({
+    host: 'localhost',
+    port: 5432,
+});
+await pgClient.connect();
+const lqlClient = new SQL(pgClient, { dialect: 'postgres' });
+
+export default async function( event, data, next ) {
     if ( next.pathname && next.stepname !== 'about' ) return next();
+    console.log('-------------------------', await lqlClient.databases());
     return {
         title: next.stepname === 'about' ? 'About - Hello World!' : 'Home - Hello World!',
     };
